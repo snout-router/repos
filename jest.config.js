d33b036905bbbf6bc6ac9b3ae899a869eef9ca62
Erin <ezzatron@gmail.com>
2021-11-30 10:14:41 +1000

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,1 @@
+export {default} from '@snout/jest-config'
---
+++ website/prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: false,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+}
