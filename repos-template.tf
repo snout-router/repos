11aa101cbb018e46e2702e28c199d0d5f18e7d51
Erin Millard <ezzatron@gmail.com>
2021-12-17 16:19:42 +1000

    Add GHA workflow files for the template repo

--- repos-template.tf
+++ repos-template.tf
@@ -28,3 +28,66 @@ resource "github_repository_file" "template_repo_license" {
   commit_message      = "Update license"
   overwrite_on_create = true
 }
+
+resource "github_repository_file" "template_repo_dot_github_workflows_ci_pr_size_yml" {
+  repository          = github_repository.template_repo.name
+  branch              = github_repository.template_repo.default_branch
+  file                = ".github/workflows/ci-pr-size.yml"
+  content             = file("workflows/ci-pr-size.yml")
+  commit_message      = "Update \"CI (PR size)\" GHA workflow"
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "template_repo_dot_github_workflows_ci_scheduled_yml" {
+  repository          = github_repository.template_repo.name
+  branch              = github_repository.template_repo.default_branch
+  file                = ".github/workflows/ci-scheduled.yml"
+  content             = file("workflows/ci-scheduled.yml")
+  commit_message      = "Update \"CI (scheduled)\" GHA workflow"
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "template_repo_dot_github_workflows_ci_website_yml" {
+  repository          = github_repository.template_repo.name
+  branch              = github_repository.template_repo.default_branch
+  file                = ".github/workflows/ci-website.yml"
+  content             = file("workflows/ci-website.yml")
+  commit_message      = "Update \"CI (website)\" GHA workflow"
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "template_repo_dot_github_workflows_ci_yml" {
+  repository          = github_repository.template_repo.name
+  branch              = github_repository.template_repo.default_branch
+  file                = ".github/workflows/ci.yml"
+  content             = file("workflows/ci.yml")
+  commit_message      = "Update \"CI\" GHA workflow"
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "template_repo_dot_github_workflows_publish_package_yml" {
+  repository          = github_repository.template_repo.name
+  branch              = github_repository.template_repo.default_branch
+  file                = ".github/workflows/publish-package.yml"
+  content             = file("workflows/publish-package.yml")
+  commit_message      = "Update \"Publish package\" GHA workflow"
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "template_repo_dot_github_workflows_publish_release_yml" {
+  repository          = github_repository.template_repo.name
+  branch              = github_repository.template_repo.default_branch
+  file                = ".github/workflows/publish-release.yml"
+  content             = file("workflows/publish-release.yml")
+  commit_message      = "Update \"Publish release\" GHA workflow"
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "template_repo_dot_github_workflows_publish_website_yml" {
+  repository          = github_repository.template_repo.name
+  branch              = github_repository.template_repo.default_branch
+  file                = ".github/workflows/publish-website.yml"
+  content             = file("workflows/publish-website.yml")
+  commit_message      = "Update \"Publish website\" GHA workflow"
+  overwrite_on_create = true
+}
