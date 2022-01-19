af949a0b2eff3e62bd215850336e2b3f6b962de9
Erin Millard <ezzatron@gmail.com>
2022-01-19 10:35:33 +1000

    Add a test repo with no template

--- repos.tf
+++ repos.tf
@@ -138,14 +138,14 @@ module "repo_tsconfig" {
   has_ci = false
 }
 
-# module "repo_test_non_template_repo" {
-#   source        = "./modules/repo"
-#   commit_author = local.commit_author
-#   commit_email  = local.commit_email
-#   license       = local.license
-#   primary_url   = local.primary_url
-#   name          = "test-non-template-repo"
-#   description   = "A repo for testing Terraform repo management"
+module "repo_test_non_template_repo_b" {
+  source        = "./modules/repo"
+  commit_author = local.commit_author
+  commit_email  = local.commit_email
+  license       = local.license
+  primary_url   = local.primary_url
+  name          = "test-non-template-repo-b"
+  description   = "A repo for testing Terraform repo management"
 
-#   has_ci = false
-# }
+  has_ci = false
+}
