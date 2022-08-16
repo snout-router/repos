11f5414b84c90a640fdb53a9f78dc8190f985f86
Erin Millard <contact@erin.id.au>
2022-08-16 22:09:18 +1000

    Decrease Dependabot interval to weekly for managed repos

---
+++ modules/repo/dependabot.tf
@@ -0,0 +1,18 @@
+resource "github_repository_file" "dot_github_dependabot_yml" {
+  count = 1
+
+  commit_author       = module.constants.committer.name
+  commit_email        = module.constants.committer.email
+  repository          = github_repository.this.name
+  branch              = github_repository.this.default_branch
+  file                = ".github/dependabot.yml"
+  commit_message      = "Update Dependabot configuration"
+  overwrite_on_create = true
+
+  content = templatefile(
+    "dot-github/dependabot.yml.tftpl",
+    {
+      has_npm_website = var.has_website && var.should_publish_website
+    }
+  )
+}
