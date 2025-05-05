0d95ebc95f33e0623909b1b7aea43fe5c9bec191
Erin Millard <contact@erin.id.au>
2025-05-05 15:05:56 +1000

    Stop managing workflows

--- modules/repo/workflows.tf
+++ modules/repo/workflows.tf
@@ -1,91 +1,55 @@
-resource "github_repository_file" "dot_github_workflows_ci_pr_size_yml" {
-  count = var.has_size_limit ? 1 : 0
+removed {
+  from = github_repository_file.dot_github_workflows_ci_pr_size_yml
 
-  commit_author       = module.constants.committer.name
-  commit_email        = module.constants.committer.email
-  repository          = github_repository.this.name
-  file                = ".github/workflows/ci-pr-size.yml"
-  commit_message      = "Update \"CI (PR size)\" GHA workflow"
-  overwrite_on_create = true
-
-  content = file("dot-github/workflows/ci-pr-size.yml")
+  lifecycle {
+    destroy = false
+  }
 }
 
-resource "github_repository_file" "dot_github_workflows_ci_scheduled_yml" {
-  count = 1
+removed {
+  from = github_repository_file.dot_github_workflows_ci_scheduled_yml
 
-  commit_author       = module.constants.committer.name
-  commit_email        = module.constants.committer.email
-  repository          = github_repository.this.name
-  file                = ".github/workflows/ci-scheduled.yml"
-  commit_message      = "Update \"CI (scheduled)\" GHA workflow"
-  overwrite_on_create = true
-
-  content = file("dot-github/workflows/ci-scheduled.yml")
+  lifecycle {
+    destroy = false
+  }
 }
 
-resource "github_repository_file" "dot_github_workflows_ci_website_yml" {
-  count = var.has_website && var.should_publish_website ? 1 : 0
+removed {
+  from = github_repository_file.dot_github_workflows_ci_website_yml
 
-  commit_author       = module.constants.committer.name
-  commit_email        = module.constants.committer.email
-  repository          = github_repository.this.name
-  file                = ".github/workflows/ci-website.yml"
-  commit_message      = "Update \"CI (website)\" GHA workflow"
-  overwrite_on_create = true
-
-  content = file("dot-github/workflows/ci-website.yml")
+  lifecycle {
+    destroy = false
+  }
 }
 
-resource "github_repository_file" "dot_github_workflows_ci_yml" {
-  count = 1
+removed {
+  from = github_repository_file.dot_github_workflows_ci_yml
 
-  commit_author       = module.constants.committer.name
-  commit_email        = module.constants.committer.email
-  repository          = github_repository.this.name
-  file                = ".github/workflows/ci.yml"
-  content             = file("dot-github/workflows/ci.yml")
-  commit_message      = "Update \"CI\" GHA workflow"
-  overwrite_on_create = true
+  lifecycle {
+    destroy = false
+  }
 }
 
-resource "github_repository_file" "dot_github_workflows_publish_package_yml" {
-  count = 1
+removed {
+  from = github_repository_file.dot_github_workflows_publish_package_yml
 
-  commit_author       = module.constants.committer.name
-  commit_email        = module.constants.committer.email
-  repository          = github_repository.this.name
-  file                = ".github/workflows/publish-package.yml"
-  commit_message      = "Update \"Publish package\" GHA workflow"
-  overwrite_on_create = true
-
-  content = file("dot-github/workflows/publish-package.yml")
+  lifecycle {
+    destroy = false
+  }
 }
 
-resource "github_repository_file" "dot_github_workflows_publish_release_yml" {
-  count = 1
+removed {
+  from = github_repository_file.dot_github_workflows_publish_release_yml
 
-  commit_author       = module.constants.committer.name
-  commit_email        = module.constants.committer.email
-  repository          = github_repository.this.name
-  file                = ".github/workflows/publish-release.yml"
-  commit_message      = "Update \"Publish release\" GHA workflow"
-  overwrite_on_create = true
-
-  content = trimsuffix(templatefile("dot-github/workflows/publish-release.yml", {
-    make_target = var.release_make_target
-  }), "\n")
+  lifecycle {
+    destroy = false
+  }
 }
 
-resource "github_repository_file" "dot_github_workflows_publish_website_yml" {
-  count = var.has_website && var.should_publish_website ? 1 : 0
+removed {
+  from = github_repository_file.dot_github_workflows_publish_website_yml
 
-  commit_author       = module.constants.committer.name
-  commit_email        = module.constants.committer.email
-  repository          = github_repository.this.name
-  file                = ".github/workflows/publish-website.yml"
-  commit_message      = "Update \"Publish website\" GHA workflow"
-  overwrite_on_create = true
-
-  content = file("dot-github/workflows/publish-website.yml")
+  lifecycle {
+    destroy = false
+  }
 }
